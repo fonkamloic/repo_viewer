04de10501101781f70197c2475d90616bc70d938
Loïc Fonkam <loicfonkam@Loics-Air.lan>
2022-10-20 11:15:48 -0400

    ./lib/auth/infrastructure/oauth2_interceptor

---
+++ lib/auth/infrastructure/oauth2_interceptor.dart
@@ -0,0 +1,47 @@
+import 'package:dio/dio.dart';
+import 'package:repo_viewer/auth/application/auth_notifier.dart';
+import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
+
+class Oauth2Interceptor extends Interceptor {
+  final GithubAuthenticator _authenticator;
+  final AuthNotifier _authNotifier;
+  final Dio _dio;
+
+  Oauth2Interceptor(this._authenticator, this._authNotifier, this._dio);
+  @override
+  Future<void> onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    final credentials = await _authenticator.getSignedInCredentials();
+    final modifiedOptions = await options
+      ..headers.addAll(
+        credentials == null
+            ? {}
+            : {'Authorization': 'bearer ${credentials.accessToken}'},
+      );
+    handler.next(modifiedOptions);
+  }
+
+  @override
+  Future<void> onError(DioError err, ErrorInterceptorHandler handler) async {
+    final errorResponse = err.response;
+    if (errorResponse != null && errorResponse.statusCode == 401) {
+      final credentials = await _authenticator.getSignedInCredentials();
+      credentials != null && credentials.canRefresh
+          ? await _authenticator.refresh(credentials)
+          : await _authenticator.clearCredentialsStorage();
+      await _authNotifier.checkAndUpdateAuthState();
+      final refeshedCredentials = await _authenticator.getSignedInCredentials();
+      if (refeshedCredentials != null) {
+        handler.resolve(await _dio.fetch(
+          errorResponse.requestOptions
+            ..headers['Authorization'] =
+                'bearer ${refeshedCredentials.accessToken}',
+        ));
+      }
+    } else {
+      handler.next(err);
+    }
+  }
+}
