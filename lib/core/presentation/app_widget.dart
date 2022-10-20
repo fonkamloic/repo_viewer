4edcd006ae751a2ab5d9802b53816c388eb1abc3
Loïc Fonkam <loicfonkam@Loics-Air.lan>
2022-10-20 11:15:48 -0400

    ./lib/core/presentation/app_widget

---
+++ lib/core/presentation/app_widget.dart
@@ -0,0 +1,56 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/auth/application/auth_notifier.dart';
+import 'package:repo_viewer/auth/shared/providers.dart';
+import 'package:repo_viewer/core/presentation/routes/app_router.gr.dart';
+import 'package:repo_viewer/core/shared/providers.dart';
+
+final initializationProvider = FutureProvider<Unit>((ref) async {
+  await ref.read(sembastProvider).init();
+  ref.read(dioProvider)
+    ..options = BaseOptions(
+      headers: {'Accept': 'application/vnd.github.v3.html+json'},
+      validateStatus: (status) =>
+          status != null && status >= 200 && status < 400,
+    )
+    ..interceptors.add(ref.read(oAuth2InterceptorProvider));
+  final authNotifier = ref.read(authNotifierProvider.notifier);
+  await authNotifier.checkAndUpdateAuthState();
+  return unit;
+});
+
+class AppWidget extends ConsumerWidget {
+  final appRouter = AppRouter();
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(initializationProvider, (previous, next) {});
+    ref.listen<AuthState>(authNotifierProvider, ((previous, next) {
+      next.maybeMap(
+        orElse: () {},
+        unauthenticated: (_) {
+          appRouter.pushAndPopUntil(const SignInRoute(),
+              predicate: (route) => false);
+        },
+        authenticated: (_) {
+          appRouter.pushAndPopUntil(const StarredReposRoute(),
+              predicate: (route) => false);
+        },
+      );
+    }));
+    return MaterialApp.router(
+      title: 'Repo Viewer',
+      theme: _setUpThemeData(),
+      routerDelegate: appRouter.delegate(),
+      routeInformationParser: appRouter.defaultRouteParser(),
+    );
+  }
+
+  ThemeData _setUpThemeData() {
+    return ThemeData(
+      primarySwatch: Colors.grey,
+      appBarTheme: AppBarTheme(backgroundColor: Colors.grey.shade50)
+    );
+  }
+}
